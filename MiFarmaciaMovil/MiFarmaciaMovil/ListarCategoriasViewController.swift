8f4e51dcc8f6a6607239bd9d08e80be812c0832a
DISEÑO <diseno@CE304-ST29.local>
2024-04-17 18:02:56 -0500

    agregado pagina de mantener categoria

---
+++ MiFarmaciaMovil/MiFarmaciaMovil/ListarCategoriasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListarCategoriasViewController.swift
+//  MiFarmaciaMovil
+//
+//  Created by DISEÑO on 17/04/24.
+//
+
+import UIKit
+
+class ListarCategoriasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
