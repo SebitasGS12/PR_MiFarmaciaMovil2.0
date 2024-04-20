e98643cf4fe5e92ebcae9defe025b109ef30b641
ad <damii@CE106-ST22.local>
2024-04-20 14:17:20 -0500

    creacion de pantalla listar producto

---
+++ MiFarmaciaMovil/MiFarmaciaMovil/ListarProductoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListarProductoTableViewCell.swift
+//  MiFarmaciaMovil
+//
+//  Created by DAMII on 20/04/24.
+//
+
+import UIKit
+
+class ListarProductoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
