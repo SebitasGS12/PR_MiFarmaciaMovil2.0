ad1c501ff5fdf7fe8826eac50bd7a1f2fd262043
sebas <sebasgoy16@gmail.com>
2024-04-21 10:23:10 -0500

    avance 5 vistas y prueba

---
+++ MiFarmaciaMovil/MiFarmaciaMovil/EmpleadoTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EmpleadoTableViewCell.swift
+//  MiFarmaciaMovil
+//
+//  Created by DAMII on 21/04/24.
+//
+
+import UIKit
+
+class EmpleadoTableViewCell: UITableViewCell {
+
+    
+    
+    
+    @IBOutlet weak var holaLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MiFarmaciaMovil/MiFarmaciaMovil/EmpleadosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmpleadosViewController.swift
+//  MiFarmaciaMovil
+//
+//  Created by DAMII on 21/04/24.
+//
+
+import UIKit
+
+class EmpleadosViewController: UIViewController {
+    
+    
+
+    @IBOutlet weak var EmpleadosTableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
